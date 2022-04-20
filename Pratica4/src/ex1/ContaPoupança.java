9271bab89c6791806fbb997fda134394624a4d59
BRF2002 <101672998+BRF2002@users.noreply.github.com>
2022-04-19 22:22:41 -0300

    Add files via upload

---
+++ Pratica4/src/ex1/ContaPoupança.java
@@ -0,0 +1,22 @@
+package ex1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class ContaPoupança extends Conta {
+
+
+		public void imprimeExtrato() {
+		System.out.println("### Extrato da Conta ###");
+		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/aaaa HH:mm:ss");
+		Date date = new Date();
+		System.out.println("Saldo: "+this.getSaldo());
+		System.out.println("Data: "+sdf.format (date));
+
+
+	}
+	
+	
+
+}
